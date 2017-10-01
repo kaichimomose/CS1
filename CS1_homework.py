18efcdae96e891afe7dec22c1de70a77e46eda95
kaichimomose <30785433+kaichimomose@users.noreply.github.com>
2017-09-30 21:08:55 -0700

    add initializ

--- CS1_homework.py
+++ CS1_homework.py
@@ -1,8 +1,15 @@
 class Classroom(object):
     """docstring fo Classroom."""
-    def __init__(self, arg):
-        self.arg = arg
+    def __init__(self, class_name, day_time, student):
+        self.class_name = class_name
+        self.day_time = day_time
+        self.student = student
+
+    def assignment(self):
+
+    def grads(self):
 
 
 class Students(object):
-    def __init__()        
+    def __init__(self, name):
+        self.name = name
